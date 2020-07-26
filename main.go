83c61c322b84fc1389f335ad81d2f5a25afaec51
Thomas Bousquet <thomas.bousquet@gmail.com>
2020-07-25 20:25:24 -0400

    added Go Modules

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {})
+	router := mux.NewRouter()
+	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {})
 
 	fmt.Printf("Starting server at port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal(err)
 	}
 }
